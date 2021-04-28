219c5756e7a2cacec28f9e988aacdfd1a2138950
Dagmara Le Grange <dlegrange3@gmail.com>
2021-04-28 14:08:04 +0100

    Initial commit

---
+++ src/main/java/MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        int num = 13;
+        for(int i = 1; i <= 30; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
